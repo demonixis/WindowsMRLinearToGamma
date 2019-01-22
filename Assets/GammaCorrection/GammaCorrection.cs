1b3779aaadcda736c6a7ffb6ee2856d0da3dc90a
Yannick Comte <comte.yannick@gmail.com>
2019-01-22 11:55:20 +0100

    Added a PostProcess Stack V2 extension
    This commit introduces an extension for the PPStack V2 that does the same thing than the older script.
    It's required if you want to use the new Scriptable Render Pipeline, or want a better optimized way if you already use the PP Stack V2.

---
+++ Assets/GammaCorrection/GammaCorrection.cs
@@ -0,0 +1,41 @@
+﻿﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.XR;
+
+namespace Demonixis.Toolbox.Graphics
+{
+	[Serializable]
+	[PostProcess(typeof(GammaCorrectionRenderer), PostProcessEvent.AfterStack, "Demonixis/GammaCorrection")]
+	public class GammaCorrection : PostProcessEffectSettings
+	{
+		public BoolParameter previewCorrection = new BoolParameter() { value = false };
+
+		public override bool IsEnabledAndSupported(PostProcessRenderContext context)
+		{
+			var result = base.IsEnabledAndSupported(context);
+			result = XRSettings.enabled;
+
+	#if UNITY_WSA
+			result = XRSettings.enabled;
+	#endif
+
+	#if UNITY_EDITOR
+			if (!UnityEditor.EditorApplication.isPlaying)
+				result = false;
+
+			result |= previewCorrection;
+	#endif
+			return result;
+		}
+	}
+
+	public sealed class GammaCorrectionRenderer : PostProcessEffectRenderer<GammaCorrection>
+	{
+		public override void Render(PostProcessRenderContext context)
+		{
+			var sheet = context.propertySheets.Get(Shader.Find("Hidden/PostProcessing/Extensions/GammaCorrection"));
+			context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+		}
+	}
+}
---
+++ Assets/GammaCorrection/GammaCorrection.shader
@@ -0,0 +1,31 @@
+﻿Shader "Hidden/PostProcessing/Extensions/GammaCorrection"
+{
+	HLSLINCLUDE
+
+	#include "PostProcessing/Shaders/StdLib.hlsl"
+
+	TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+
+	float4 Frag(VaryingsDefault i) : SV_Target
+	{
+		float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoordStereo);
+		return pow(color, 0.454545);
+	}
+
+	ENDHLSL
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			HLSLPROGRAM
+
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+
+			ENDHLSL
+		}
+	}
+}
